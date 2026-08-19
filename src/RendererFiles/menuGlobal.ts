5f4af558c7b5bd7306b5e732cc67a381cc4eb299
Luthetus <luthetus@gmail.com>
2026-08-18 20:27:39 -0400

    analysis paralysis at the grocery store lol

--- src/RendererFiles/menuGlobal.ts
+++ src/RendererFiles/menuGlobal.ts
@@ -529,5 +529,7 @@ but there's plant iron and meat iron and they're not the same thing
 And the numbers just don't add up in my head in any way I think I'm just trippin though.
 
 I don't know if I'm eating enough
+
+analysis paralysis at the grocery store lol
 */
 
