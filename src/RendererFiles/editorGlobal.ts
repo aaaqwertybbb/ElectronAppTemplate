9b729cd0b367f899b0740c09725fb285b400bd8c
Luthetus <luthetus@gmail.com>
2026-08-23 13:40:42 -0400

    - problematic case is that you can't rebind paste event

--- src/RendererFiles/editorGlobal.ts
+++ src/RendererFiles/editorGlobal.ts
@@ -5362,6 +5362,7 @@ function EDITOR_onKeyDown_PageUp(event: KeyboardEvent) {
  *     - Solutions:
  *         - The synchronous paste event
  *             - side note you always wondered why they focused a textarea, in part it is from what I understand so you can paste into it and synchronously get the pasted text.
+ *             - problematic case is that you can't rebind paste event
 */
 async function EDITOR_onKeyDown_keyLengthEqualsOne_ctrlKey(event: KeyboardEvent) {
     let indexCursor = 0; // TODO: Actually get the correct indexCursor instead of just hardcoding '0'
